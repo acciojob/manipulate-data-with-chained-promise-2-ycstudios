a7251f50fbf2cbb7daf02032654a0d6d0079eb76
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-03-05 19:37:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const output = document.getElementById('output');
+
+let arr = [1, 2, 3, 4];
+
+let p1 = new Promise((resolve) => 
+    setTimeout(() => {
+        resolve(arr);
+    }, 3000)
+).then((arr) => {
+    return new Promise((resolve) => {
+        const filteredResult = arr.filter((value) => value % 2 === 0);
+        setTimeout(() => {
+            output.innerHTML = ` ${filteredResult}`;
+            resolve(filteredResult);
+        }, 1000);
+    });
+}).then((filteredResult) => {
+    return new Promise((resolve) => {
+        const multipliedResult = filteredResult.map((value) => value * 2);
+        setTimeout(() => {
+            output.innerHTML = ` ${multipliedResult}`;
+            resolve(multipliedResult);
+        }, 2000);
+    });
+})
